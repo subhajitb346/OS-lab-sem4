b02266e7653e80d2f2dc735d4cd16be9a9340d9e
subhajitb346 <subhojitb67@gmail.com>
2026-04-26 22:56:52 +0530

    Add new storage device files in Secondary directory

---
+++ treeStructure.sh
@@ -0,0 +1,21 @@
+mkdir memory_13
+cd memory_13
+
+mkdir Primary Secondary
+
+cd Primary
+mkdir rom ram
+
+cd rom
+touch prom eprom eeprom
+cd ..
+
+cd ram
+touch dram sram
+cd ..
+
+cd ..
+
+cd Secondary
+touch hdd ssd odd "USB Flash Drive"
+
